33a2e84b356a383d077c05c8c6fe6ad9bca74fe0
TUF GAMING F15 <iftikharazharchaudhry@gmail.com>
2025-12-14 14:29:22 +0700

    hhh

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Neoroza Dashboard</title>
+    <title>Neoroza Contact</title>
     <link href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.0/css/all.min.css" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
